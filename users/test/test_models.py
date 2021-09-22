44dea40f2443e94fbc87f5b01499df876400cf2d
Blankxx420 <briceguichou.dev@gmail.com>
2021-09-22 16:22:13 +0200

    creating test for user model

---
+++ users/test/test_models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email="normaluser@gmail.com", password="foo")
+        self.assertEqual(user.email, "normaluser@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="foo")
+
