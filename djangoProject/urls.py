aa2d7ef916ea31016b570a92ae11f135768acd2e
Blankxx <briceguichou.dev@gmail.com>
2021-11-02 18:54:36 +0100

    cahnging path url

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 import search.urls
 
 urlpatterns = [
+    path('', include(search.urls)),
     path('admin/', admin.site.urls),
-    path('', include(search.urls))
 ]
