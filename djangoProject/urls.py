67b9d63a920b4785b35635e962fbf25ca42ea68b
Blankxx <briceguichou.dev@gmail.com>
2021-11-02 18:44:20 +0100

    cahnging path url

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import search.urls
+from search.views import home
 
 urlpatterns = [
+    path('/', home),
     path('admin/', admin.site.urls),
     path('search/', include(search.urls))
 ]
