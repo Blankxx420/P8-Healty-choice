a4efe2a6adde5734fd5709e9ec4b9dd5eaa2bf6d
blankxx <Shinima16>
2021-12-06 01:25:03 +0100

    ajout des models user sur le site admin

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,11 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 import search.urls
-import users.urls
+from users import views as users_views
 
 urlpatterns = [
     path('', include(search.urls)),
     path('admin/', admin.site.urls),
-    path('login/', include(users.urls))
+    path("register/", users_views.register, name="register"),
+    path("account/", users_views.account, name="account"),
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
 ]
