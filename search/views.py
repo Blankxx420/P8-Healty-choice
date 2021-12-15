98bc5c97bcf53005c90809f07598151964870831
blankxx <Shinima16>
2021-12-15 15:57:22 +0100

    creating views for substitutes.html

--- search/views.py
+++ search/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Count
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from search.search_form import Searchbar
 from search.models.product import Product
+from search.models.category import Categories
 
 
 def home(request):
@@ -31,6 +34,7 @@ def products(request):
 def product(request, product_id):
     """Displays the product details page
     Args:
+        request: base parameter
         product_id (int): Id of the product
     """
     # try:
@@ -41,5 +45,31 @@ def product(request, product_id):
     return render(request, "search/product.html", context)
 
 
-def legal_notice():
-    return render("search/legal_notice.html")
+def substitutes(request, product_id):
+    """ Display substitutes from selected product"""
+    # Find product searched by user with id
+    product_query = Product.objects.get(pk=product_id)
+
+    # Find the category of the product
+    product_query_cat = Categories.objects.filter(product_id=product_query.id)
+
+    # Find 9 products with better nutrition_score in the same category
+    substitutes_prod = (
+        Product.objects.filter(categories__in=product_query_cat)
+        .annotate(nb_cat=Count("categories"))
+        .filter(nb_cat__gte=3)
+        .filter(nutriscore__lt=product_query.nutrition_score)
+        .order_by("nutrition_score")[:9]
+    )
+
+    context = {"product": product_query, "substitutes": substitutes_prod}
+
+    return render(request, "search/substitutes.html", context)
+
+
+@login_required
+def save_favorite
+
+
+def legal_notice(request):
+    return render(request, "search/legal_notice.html")
