235d50914e75f650d9bea1887bddfb462e22dee9
blankxx <Shinima16>
2022-01-31 13:49:55 +0100

    preparing deployment

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -96,10 +96,7 @@ DATABASES = {}
 
 if os.getenv("DATABASE_URL"):
     DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 else:
     DATABASES = {
